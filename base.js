43fb51a7f92d50442caaca233748d899f7ec9160
web-best <39441464+web-best@users.noreply.github.com>
2018-08-13 01:30:39 +0900

    Add files via upload

---
+++ base.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	var w_width = $(window).width();
+	var w_height = $(window).height();
+	var w_STop = $(window).scrollTop();
+	
+	//$('.con-wrap').css('margin-top',$('.header').height());
+});

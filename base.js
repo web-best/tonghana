d0f3cd28dcfe8d73f30c67e2fc4ab0b339002e02
web-best <39441464+web-best@users.noreply.github.com>
2018-08-15 17:33:10 +0900

    Add files via upload

--- base.js
+++ base.js
@@ -3,5 +3,21 @@ $(document).ready(function(){
 	var w_height = $(window).height();
 	var w_STop = $(window).scrollTop();
 	
+	$(window).scroll(function(e) {
+        
+    });
+	
+	//$('#menu').click(function(){
+//		$(this).toggleClass({
+//            
+//        });
+//	});
+	
 	//$('.con-wrap').css('margin-top',$('.header').height());
+	
+	console.log(w_width)
+	console.log(w_height)
+	console.log(w_STop)
+	
+	
 });
